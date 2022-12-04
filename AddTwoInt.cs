b4a014d82e5324bb656b8fde59abedaa4ab2e323
cricket5225 <111393981+cricket5225@users.noreply.github.com>
2022-12-03 20:44:51 -0500

    Add files via upload

---
+++ AddTwoInt.cs
@@ -0,0 +1,11 @@
+using System;
+/* LeetCode Easy
+Given two integers num1 and num2, return the sum of the two integers.
+
+*/
+public class Solution {
+    public int Sum(int num1, int num2) {
+        return num1 + num2;
+    }
+}
+
